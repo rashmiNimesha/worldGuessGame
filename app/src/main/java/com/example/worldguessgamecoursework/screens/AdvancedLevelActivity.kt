f7d46b2fdd1c523574adcba794ebf3a00ee106f4
rashmiNimesha <90458280+rashmiNimesha@users.noreply.github.com>
2024-04-03 03:17:49 +0530

    advanced

--- app/src/main/java/com/example/worldguessgamecoursework/screens/AdvancedLevelActivity.kt
+++ app/src/main/java/com/example/worldguessgamecoursework/screens/AdvancedLevelActivity.kt
@@ -16,8 +16,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -31,11 +33,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.worldguessgamecoursework.R
+import com.example.worldguessgamecoursework.data.buttonFontSize
+import com.example.worldguessgamecoursework.data.themeColor
 
 class AdvancedLevelActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,27 +59,51 @@ fun AdvancedLevelScreen(){
     val (guesses, setGuesses) = remember { mutableStateOf(List(flagOptions.size) { "" }) }
     var msg by remember { mutableStateOf("") }
     var submit by remember {mutableStateOf(false)}
+    var submitCount by remember { mutableStateOf(0) }
+    var submitText by remember { mutableStateOf("Submit") }
+    var allGuess by remember { mutableStateOf(0) }
+    var correctGuess by remember { mutableStateOf(0) }
+    var correct by remember { mutableStateOf("") }
+    
 
     fun checkAnswers() {
         var allCorrect = true
         flagOptions.indices.forEach { index ->
             if (!flagOptions[index].flagName.equals(guess[index], ignoreCase = true)) {
                 allCorrect = false
+            }else{
+                correctGuess++
             }
         }
         submit = false
-        msg = if (allCorrect) {
-            "Correct"
+        if (allCorrect) {
+            msg ="Correct !"
+            submitCount = 1
+           correctGuess++
+            guess = List(flagOptions.size) { "" }
+            flagOptions = generateRandomFlags_()
         }
         else{
-            "Wrong"
+            submitCount++
+            if (submitCount == 3) {
+                submitText = "Next"
+
+
+                // Reset guesses for new game
+//                guess = List(flagOptions.size) { "" }
+//                flagOptions = generateRandomFlags_()
+            }
+            msg = "Wrong !"
+            
         }
+
+        allGuess++
     }
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .padding(18.dp),
-        horizontalAlignment = Alignment.CenterHorizontally){
+        horizontalAlignment = Alignment.CenterHorizontally) {
 
         item {
             Row(
@@ -98,28 +127,30 @@ fun AdvancedLevelScreen(){
                 Spacer(
                     modifier = Modifier.weight(1f)
                 )
-                Image(
-                    modifier = Modifier
-                        .size(80.dp)
-                        .padding(10.dp),
-                    painter = painterResource(id = R.drawable.ad),
-                    contentDescription = ""
-                )
+
+                Text(text = "$correctGuess / $allGuess",
+                    modifier = Modifier.padding(10.dp),
+                    color = Color.Black,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,)
 
             }
         }
 
         item {
-            Row (modifier = Modifier.fillMaxWidth(),
+            Row(
+                modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.Center
-            ){
-                Text(text = "Play and Win, Home Page",
+            ) {
+                Text(
+                    text = "Play and Win, Home Page",
                     color = Color(0xFF75A488),
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier
                         .padding(5.dp)
-                        .align(alignment = Alignment.CenterVertically))
+                        .align(alignment = Alignment.CenterVertically)
+                )
 
             }
         }
@@ -129,7 +160,7 @@ fun AdvancedLevelScreen(){
 
             Row {
 
-                flagOptions.forEachIndexed{index, flag_->
+                flagOptions.forEachIndexed { index, flag_ ->
                     FlagImageAdvanced(flag = flag_) {
 
                     }
@@ -140,19 +171,25 @@ fun AdvancedLevelScreen(){
         }
 
         item {
-            Row (modifier = Modifier.padding(16.dp)){
-                Text(text = "(1)",
+            Row(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = "(1)",
                     modifier = Modifier.padding(end = 56.dp, start = 36.dp),
                     fontSize = 17.sp,
-                    fontWeight = FontWeight.Black)
-                Text(text = "(2)",
+                    fontWeight = FontWeight.Black
+                )
+                Text(
+                    text = "(2)",
                     modifier = Modifier.padding(end = 56.dp, start = 46.dp),
                     fontSize = 17.sp,
-                    fontWeight = FontWeight.Black)
-                Text(text = "(3)",
-                    modifier = Modifier.padding( start = 40.dp),
+                    fontWeight = FontWeight.Black
+                )
+                Text(
+                    text = "(3)",
+                    modifier = Modifier.padding(start = 40.dp),
                     fontSize = 17.sp,
-                    fontWeight = FontWeight.Black)
+                    fontWeight = FontWeight.Black
+                )
             }
         }
 
@@ -174,7 +211,7 @@ fun AdvancedLevelScreen(){
                         onValueChange = { newValue ->
 
                             // Update guess
-                            if(!submit){
+                            if (!submit) {
                                 guess = guess.toMutableList().also { it[index] = newValue }
                             }
 
@@ -182,45 +219,81 @@ fun AdvancedLevelScreen(){
                         modifier = Modifier
                             .padding(bottom = 20.dp)
                             .background(
-                                if (submit && guess[index].equals(
+                                if (guess[index].equals(
                                         flagOptions[index].flagName,
-                                        ignoreCase = false
+                                        ignoreCase = true
                                     )
-                                ) Color.LightGray else Color.Red
+                                ) Color.Gray else Color.White
                             )
-
                             .border(
                                 BorderStroke(
-                                    width = 1.dp,
-                                    color = if (submit && guess[index].equals(
+                                    1.dp,
+                                    if (guess[index].equals(
                                             flagOptions[index].flagName,
                                             ignoreCase = true
                                         )
-                                    ) Color.Green else Color.Red
+                                    )
+                                        Color.Green else Color.Red
+
                                 )
 
-                            ))
-
-
-//
+                            )
+                            .padding(horizontal = 8.dp, vertical = 8.dp)
+                            .width(200.dp),
+                        enabled = !guess[index].equals(
+                            flagOptions[index].flagName,
+                            ignoreCase = true
+                        )
+                    )
 
                 }
             }
         }
 
         item {
-            Button(onClick = {
-                checkAnswers()
-            }) {
-                Text(text = "Submit")
+            Button(
+                onClick = {
+                    if (submitText == "Submit") {
+                        checkAnswers()
+
+                    } else {
+                        submitText = "Submit"
+                        submitCount = 0
+                        msg = ""
+                        guess = List(flagOptions.size) { "" }
+                        flagOptions = generateRandomFlags_()
+                    }
+
+                },
+                colors = ButtonDefaults.buttonColors(themeColor),
+                modifier = Modifier
+                    .padding(2.dp)
+                    .width(150.dp)
+                    .height(40.dp)
+
+            ) {
+                Text(
+                    text = submitText,
+                    fontSize = buttonFontSize,
+                    style = TextStyle(fontWeight = FontWeight.Bold),
+                    color = Color.White
+                )
             }
         }
 
         item {
-            Text(text = msg)
+            Spacer(modifier = Modifier.height(15.dp))
+            Text(
+                text = msg,
+                color = if (msg == "Correct !") Color.Green else Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+            )
+
         }
 
 
-    }
 
+    }
 }
+
